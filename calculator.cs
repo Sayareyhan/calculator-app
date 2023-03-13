97907cd2ba2b4e5936044ffea6a4a76bb5d2f35b
sayareyhan <reyhanarya@students.amikom.ac.id>
2023-03-13 16:58:43 +0700

    tambah file melengkapi fungsi main

--- calculator.cs
+++ calculator.cs
@@ -2,7 +2,14 @@ class calculator
 {
 	static void main(string[] args)
 	{
+	  int a = 12;
+	  int b = 3;
+	 
+		console.WriteLine("hasil penambahan: {0} + {1} = {2}", a, b, penambahan(a,b));
+		console.WriteLine("hasil penambahan: {0} - {1} = {2}", a, b, pengurangan(a,b));
 	
+		console.WriteLine("\nTekan sembarang nek arep metu");
+		console.Readkey();
 	}
 	
 	static int penambahan(int a, int b)
